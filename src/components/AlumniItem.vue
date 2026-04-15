0ceb2ee6ac0849ac400a4b37b113fe653bfff30b
noodle <nonomi2603@gmail.com>
2026-04-15 19:19:44 +0500

    Fix alumi sexion

--- src/components/AlumniItem.vue
+++ src/components/AlumniItem.vue
@@ -1,59 +1,86 @@
 <template>
   <div class="graduate-card">
     <div class="image-box">
-      <img :src="images[props.type] || images.saule" :alt="memberText.name">
+      <img :src="images[props.type] || images.saule" :alt="displayName">
     </div>
     <div class="text-box">
-      <h3 class="name">{{ memberText.name }}</h3>
+      <h3 class="name">{{ displayName }}</h3>
     </div>
   </div>
 </template>
 
 <script setup>
 import { computed } from 'vue';
-import { useI18n } from 'vue-i18n';
 
-const { tm, rt } = useI18n();
 const props = defineProps(['type']);
 
 const images = {
   yerzhight:    '/images/alumin/YERZHIGIT NABIYEV.png',
-  marina:    '/images/alumin/MARINA ORYNBAYEVA.png',
-  adar: '/images/alumin/AIDAR KAKABAYEV.png',
-  maya:    '/images/alumin/MAYA KAIGORODSEVA.png',
-  almas:    '/images/alumin/Almas Mukhametov.png',
-  dauren:   '/images/alumin/DAUREN ZHAKAY.png',
-  nurbakhyt:  '/images/alumin/NURBAKHYT NURBAYEV.png',
-  assel:   '/images/alumin/ASSEL BADINA .png',
-  karimzhan:   '/images/alumin/KARIMZHAN SULEIMANOV .png',
-  aruzhan:   '/images/alumin/ARUZHAN KARIMOVA .png',
-  ALIZHAN:   '/images/alumin/ALIZHAN NURGAZY.png',
-  VLADISLAV:   '/images/alumin/VLADISLAV PERsHiN.png',
-  ALIYA:   '/images/alumin/ALIYA RYSBEKOVA.png',
-  MUKAGALI:   '/images/alumin/MUKAGALI IKRAMOV .png',
-  TEMIRLAN:   '/images/alumin/TEMIRLAN ABZHANOV.png',
-  DIANA:   '/images/alumin/DIANA NURZHANova.png',
-  ALINA:   '/images/alumin/ALINA BEGIMOVA.png',
-  BEKZHAN:   '/images/alumin/BEKZHAN SERIKKALIYEV.png',
-  BENAZIR:   '/images/alumin/BENAZIR MAMYTOVA.png',
-  SULTAN:   '/images/alumin/sultan mukhamedkaliev.png',
-  ANEL:   '/images/alumin/ANEL KOBEY.png',
-  SULEIMEN:   '/images/alumin/Gulsim Aitkhozhaeva.JPG',
-  NAZERKE:   '/images/alumin/NAZERKE ORYNKHAN .png',
-  DAURZHAN:   '/images/alumin/Daurzhan Augambau .png',
-  TEMIRLAN2:   '/images/alumin/TEMIRLAN BERKIMBAYEV.png',
-  ULYKZHAN:   '/images/alumin/ulykzhan Kakharmanuly.png',
-  AMINA:   '/images/alumin/AMINA KENES.png',
-  DANIYAL:   '/images/alumin/Daniyal KAKABAYEV.png',
-  AIKENZHE:   '/images/alumin/Aikenzhe Zhassulanova.png',
+  marina:       '/images/alumin/MARINA ORYNBAYEVA.png',
+  adar:         '/images/alumin/AIDAR KAKABAYEV.png',
+  maya:         '/images/alumin/MAYA KAIGORODSEVA.png',
+  almas:        '/images/alumin/Almas Mukhametov.png',
+  dauren:       '/images/alumin/DAUREN ZHAKAY.png',
+  nurbakhyt:    '/images/alumin/NURBAKHYT NURBAYEV.png',
+  assel:        '/images/alumin/ASSEL BADINA .png',
+  karimzhan:    '/images/alumin/KARIMZHAN SULEIMANOV .png',
+  aruzhan:      '/images/alumin/ARUZHAN KARIMOVA .png',
+  ALIZHAN:      '/images/alumin/ALIZHAN NURGAZY.png',
+  VLADISLAV:    '/images/alumin/VLADISLAV PERsHiN.png',
+  ALIYA:        '/images/alumin/ALIYA RYSBEKOVA.png',
+  MUKAGALI:     '/images/alumin/MUKAGALI IKRAMOV .png',
+  TEMIRLAN:     '/images/alumin/TEMIRLAN ABZHANOV.png',
+  DIANA:        '/images/alumin/DIANA NURZHANova.png',
+  ALINA:        '/images/alumin/ALINA BEGIMOVA.png',
+  BEKZHAN:      '/images/alumin/BEKZHAN SERIKKALIYEV.png',
+  BENAZIR:      '/images/alumin/BENAZIR MAMYTOVA.png',
+  SULTAN:       '/images/alumin/sultan mukhamedkaliev.png',
+  ANEL:         '/images/alumin/ANEL KOBEY.png',
+  SULEIMEN:     '/images/alumin/SULEIMEN MAMEN.png',
+  NAZERKE:      '/images/alumin/NAZERKE ORYNKHAN .png',
+  DAURZHAN:     '/images/alumin/Daurzhan Augambau .png',
+  TEMIRLAN2:    '/images/alumin/TEMIRLAN BERKIMBAYEV.png',
+  ULYKZHAN:     '/images/alumin/ulykzhan Kakharmanuly.png',
+  AMINA:        '/images/alumin/AMINA KENES.png',
+  DANIYAL:      '/images/alumin/Daniyal KAKABAYEV.png',
+  AIKENZHE:     '/images/alumin/Aikenzhe Zhassulanova.png',
 };
 
-const memberText = computed(() => {
-  const allMembers = tm('council.members');
-  const data = allMembers[props.type] || allMembers.saule;
-  return {
-    name: rt(data.name),
-  };
+const alumniData = {
+  yerzhight: "YERZHIGIT NABIYEV",
+  marina: "MARINA ORYNBAYEVA",
+  adar: "AIDAR KAKABAYEV",
+  maya: "MAYA KAIGORODSEVA",
+  almas: "ALMAS MUKHAMETOV",
+  dauren: "DAUREN ZHAKAY",
+  nurbakhyt: "NURBAKHYT NURBAYEV",
+  assel: "ASSEL BADINA",
+  karimzhan: "KARIMZHAN SULEIMANOV",
+  aruzhan: "ARUZHAN KARIMOVA",
+  ALIZHAN: "ALIZHAN NURGAZY",
+  VLADISLAV: "VLADISLAV PERSHIN",
+  ALIYA: "ALIYA RYSBEKOVA",
+  MUKAGALI: "MUKAGALI IKRAMOV",
+  TEMIRLAN: "TEMIRLAN ABZHANOV",
+  DIANA: "DIANA NURZHANOVA",
+  ALINA: "ALINA BEGIMOVA",
+  BEKZHAN: "BEKZHAN SERIKKALIYEV",
+  BENAZIR: "BENAZIR MAMYTOVA",
+  SULTAN: "SULTAN MUKHAMEDKALIEV",
+  ANEL: "ANEL KOBEY",
+  SULEIMEN: "SULEIMEN MAMEN",
+  NAZERKE: "NAZERKE ORYNKHAN",
+  DAURZHAN: "DAURZHAN AUGAMBAY",
+  TEMIRLAN2: "TEMIRLAN BERKIMBAYEV",
+  ULYKZHAN: "ULYKZHAN KAKHARMANULY",
+  AMINA: "AMINA KENES",
+  DANIYAL: "DANIYAL KAKABAYEV",
+  AIKENZHE: "AIKENZHE ZHASSULANOVA",
+};
+
+const displayName = computed(() => {
+  // Ищем имя в нашем объекте, если нет — выводим пустую строку или заглушку
+  return alumniData[props.type] || "Graduate";
 });
 </script>
 
@@ -62,7 +89,7 @@ const memberText = computed(() => {
   display: flex;
   align-items: center;
   gap: 16px;
-  min-width: 240px;
+  min-width: 320px; /* Немного увеличил, чтобы длинные имена не сжимались */
   background: white;
   padding: 10px;
 }
@@ -85,5 +112,6 @@ const memberText = computed(() => {
   font-weight: 800;
   margin: 0;
   color: #333;
+  text-transform: uppercase; /* Чтобы все имена были в одном стиле, как в списке */
 }
 </style>
